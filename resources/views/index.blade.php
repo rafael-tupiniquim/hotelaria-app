27aa805498b38f3afb61ee49d10458de93325c06
rafael-tupiniquim <124164278+rafael-tupiniquim@users.noreply.github.com>
2023-06-25 10:11:04 -0300

    first-commit
    config_ambiente

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('templates.template')
+
+@section('content')
+    <h1 class="text-center">Crud com Laravel - Hotelaria</h1>
+    <hr>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', 'BookController@index');
+Route::get('/', [BookController::class, 'index']);
